74588dc51f9bf0a1c9fcb80811fff46974a1b721
Yavras <226085@student.pwr.edu.pl>
2019-06-29 12:37:04 +0200

    First test failed

--- test/calculator/StringCalculatorTest.java
+++ test/calculator/StringCalculatorTest.java
@@ -4,9 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringCalculatorTest {
+class StringCalculatorTest
+{
+    // first test point
 
     @Test
-    void add() {
+    public void methodShouldReturnZeroOnEmptyString()
+    {
+        assertEquals(0, StringCalculator.add(""));
+    }
+
+    @Test
+    public void methodShouldReturnItself()
+    {
+        assertEquals(8, StringCalculator.add("8"));
+    }
+
+    @Test
+    public void methodShouldReturnSumOfTwoNumbers()
+    {
+        assertEquals(10, StringCalculator.add("3,7"));
     }
 }
