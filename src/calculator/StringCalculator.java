4e39bd8d8ae08780f276f324e60dc12a5e959060
Yavras <226085@student.pwr.edu.pl>
2019-06-30 14:59:48 +0200

    Sixth test passed

--- src/calculator/StringCalculator.java
+++ src/calculator/StringCalculator.java
@@ -15,7 +15,9 @@ public class StringCalculator {
     public static final String commaAndNewLineRegex = "(.)[,](.)|(.)\\n(.*)";
     public static final Pattern differentDelimiterPattern = Pattern.compile("//(.*)\n(.*)");
     public static final Pattern negativesRegex = Pattern.compile("(-[0-9]+)");
+    public static final String findBigNumbersRegex = "[1-9]\\d{3,}";
     public static int add(String text) throws Exception {
+        text=text.replaceAll(findBigNumbersRegex,"0");
         boolean valid = text.matches(commaAndNewLineRegex);
         Matcher negatives = negativesRegex.matcher(text);
         boolean negativeCondition = negatives.find();
