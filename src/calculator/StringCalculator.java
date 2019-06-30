d694560047f310a96c733cfb312ed969e1a95530
Yavras <226085@student.pwr.edu.pl>
2019-06-30 17:33:58 +0200

    Some refactor and clean code.

--- src/calculator/StringCalculator.java
+++ src/calculator/StringCalculator.java
@@ -12,55 +12,72 @@ import static java.util.Arrays.stream;
 
 public class StringCalculator {
 
-    public static final String commaAndNewLineRegex = "(.)[,](.)|(.)\\n(.*)";
+    public static final String commaOrNewLineRegex = "(.)[,](.)|(.)\\n(.*)";
     public static final Pattern differentDelimiterPattern = Pattern.compile("//(.*)\n(.*)");
     public static final Pattern negativesRegex = Pattern.compile("(-[0-9]+)");
     public static final String findBigNumbersRegex = "(100[1-9]|10[1-9][0-9]|1[1-9][0-9]{2}|[2-9][0-9]{3,})";
 
     public static int add(String text) throws Exception {
+
         text=text.replaceAll(findBigNumbersRegex,"0");
-        boolean valid = text.matches(commaAndNewLineRegex);
+
+        boolean commaOrNewLineCondition = text.matches(commaOrNewLineRegex);
         Matcher negatives = negativesRegex.matcher(text);
-        boolean negativeCondition = negatives.find();
-        if(negativeCondition) {
-            negatives.reset();
-            List<String> allMatches = new ArrayList<String>();
-            while (negatives.find())
-            {
-                allMatches.add(negatives.group());
-            }
-            throw new Exception("negatives not allowed: " + allMatches.toString());
+        boolean negativeFound = negatives.find();
+        if(negativeFound) {
+            return findNegativesAndThrowException(negatives);
         }
         if(text.isEmpty()){
             return 0;
-        }  else if(valid) {
-            String[] parts = text.split(",|\\n");
-            int sum=0;
-            for (String part : parts)
-            {
-                sum = sum + Integer.parseInt(part);
-            }
-            return sum;
+        }  else if(commaOrNewLineCondition) {
+            return splittedByCommaOrNewLine(text);
 
         } else if(text.startsWith("//")) {
-            String delimiterRegex, numbers;
-            final Matcher matcher = differentDelimiterPattern.matcher(text);
-            if(matcher.find()){
-                delimiterRegex = matcher.group(1);
-                numbers = matcher.group(2);
-
-            }else {
-                throw new IllegalArgumentException();
-            }
-            final String[] ints = numbers.split((Pattern.quote(delimiterRegex)));
-            return stream(ints)
-                    .mapToInt(Integer::parseInt)
-                    .sum();
+            return splittedByDifferentDelimiterPattern(text);
         }
         else {
             return Integer.parseInt(text);
         }
 
+    }
+
+
+    private static int findNegativesAndThrowException(Matcher negatives) throws Exception {
+        negatives.reset();
+        List<String> allMatches = new ArrayList<String>();
+        while (negatives.find())
+        {
+            allMatches.add(negatives.group());
+        }
+        throw new Exception("negatives not allowed: " + allMatches.toString());
+    }
+
+
+    private static int splittedByCommaOrNewLine(String text) {
+        String[] parts = text.split(",|\\n");
+        int sum=0;
+        for (String part : parts)
+        {
+            sum = sum + Integer.parseInt(part);
+        }
+        return sum;
+    }
+
+
+    private static int splittedByDifferentDelimiterPattern(String text) {
+        String delimiterRegex, numbers;
+        final Matcher matcher = differentDelimiterPattern.matcher(text);
+        if(matcher.find()){
+            delimiterRegex = matcher.group(1);
+            numbers = matcher.group(2);
+
+        }else {
+            throw new IllegalArgumentException();
+        }
+        final String[] ints = numbers.split((Pattern.quote(delimiterRegex)));
+        return stream(ints)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
 }
